98ca294c230462b6ead9ea4d1cccf43ea7fa794f
Owusu kelvin Clark <kelvinclark2k77@gmail.com>
2021-07-27 12:48:17 +0100

    :package: Create HourlyWeatherListItem component
    Component is also not yet styled. This is just a base implementation
    from imagination 😂

---
+++ components/HourlyWeatherListItem.js
@@ -0,0 +1,17 @@
+import { Image } from "next/image";
+const HourlyWeatherListItem = (props) => {
+  return (
+    <div>
+      <h3>{props.hour}</h3>
+      <Image
+        src={props.weatherIcon}
+        width={50}
+        height={50}
+        alt="Weather Icon"
+      />
+      <h5>{props.temperatur}</h5>
+    </div>
+  );
+};
+
+export default HourlyWeatherListItem;
