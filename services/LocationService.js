bb1d219b6752f7abacd3ac94b2096029f78e41f3
Owusu kelvin Clark <kelvinclark2k77@gmail.com>
2021-07-30 03:48:41 +0100

    :construction: Get user location
    Created a function to get user location

---
+++ services/LocationService.js
@@ -0,0 +1,21 @@
+function getLocationData() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (postion) => {
+        return {
+          latitude: postion.coords.latitude,
+          longitude: postion.coords.longitude,
+        };
+      },
+      (_) => {
+        return {
+          error: "Error fetching location",
+        };
+      }
+    );
+  } else {
+    return {
+      error: "Location service not supported",
+    };
+  }
+}
