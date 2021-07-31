b1318bf35d28999f0bfc0748854dab3e198b1a9e
Owusu kelvin Clark <kelvinclark2k77@gmail.com>
2021-07-31 11:34:06 +0100

    :lipstick: Style elements
    Did initial styling of elements on the page. Now
    mosted of the items are styled not all though.

--- components/HourlyWeatherList.js
+++ components/HourlyWeatherList.js
@@ -1,16 +1,41 @@
 import HourlyWeatherListItem from "./HourlyWeatherListItem";
+import style from "../styles/HourlyList.module.css";
 
 const HourlyWeatherList = ({ hourlyData }) => {
   return (
-    <div>
-      {hourlyData.hourly.map((weatherData) => (
-        <HourlyWeatherListItem
-          key={weatherData.hour}
-          hour={weatherData.hour}
-          weatherIcon={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
-          temperature={weatherData.temperature}
-        />
-      ))}
+    <div className={style.flexContainer}>
+      <div
+        className={`${style.nav} ${style.navLeft}`}
+        onClick={() => {
+          document.getElementById("weather-list").scrollBy({
+            left: -50,
+            behavior: "smooth",
+          });
+        }}
+      >
+        {"<"}
+      </div>
+      <div id="weather-list" className={style.weatherList}>
+        {hourlyData.hourly.map((weatherData) => (
+          <HourlyWeatherListItem
+            key={weatherData.dt}
+            hour={weatherData.hour}
+            weatherIcon={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+            temperature={weatherData.temperature}
+          />
+        ))}
+      </div>
+      <div
+        className={`${style.nav} ${style.navRight}`}
+        onClick={() => {
+          document.getElementById("weather-list").scrollBy({
+            left: +50,
+            behavior: "smooth",
+          });
+        }}
+      >
+        {">"}
+      </div>
     </div>
   );
 };
