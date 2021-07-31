43f23573a5b204bd9836dd55c672896e52b5ad00
Owusu kelvin Clark <kelvinclark2k77@gmail.com>
2021-07-31 15:46:30 +0100

    :building_construction: Fetch weather data by city name

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -5,17 +5,20 @@ import getRandInt from "../services/RandomNumberGenerator";
 const SearchBar = ({ query, setQuery, submitHandler }) => {
   const cities = ["Accra,GH", "Fes,MA", "Ontario,CA", "Britol,ENG"];
   return (
-    <form className={style.formGroup}>
+    <form
+      className={style.formGroup}
+      onSubmit={(e) => {
+        e.preventDefault();
+        submitHandler();
+      }}
+    >
       <input
         className={style.searchBox}
         type="search"
         placeholder={cities[getRandInt(0, 4)]}
         value={query}
-        onSubmit={(e) => {
-          e.preventDefault();
-          submitHandler;
-        }}
         onChange={(e) => setQuery(e.target.value)}
+        required
       />
       <button className={style.btn}>
         <Image src="/search.svg" width={20} height={20} alt="Search Button" />
